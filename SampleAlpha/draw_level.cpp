3ec5d8392050a43cbc223bf91af1bc09002dec79
Zhunhang <tayzhunhang@gmail.com>
2023-01-28 18:56:34 +0800

    updated drawlevel pt1

--- SampleAlpha/draw_level.cpp
+++ SampleAlpha/draw_level.cpp
@@ -1,22 +1,22 @@
 #include "AEEngine.h"
-
+#include "Utilities.hpp"
 
 AEGfxTexture* rect;
 AEGfxVertexList* rectmesh;
 
 void draw_level_init() {
 
-	rect = AEGfxTextureLoad("Assets/rect.png");
+	rect = AEGfxTextureLoad("Assets/simplified_png/PNG/Tiles/platformPack_tile001.png");
 
 	AEGfxMeshStart();
 	AEGfxTriAdd(
-		0.0f, 75.0f, 0xFFFFFFFF, 0.0f, 0.0f,
-		300.0f, 75.0f, 0xFFFFFFFF, 0.0f, 0.0f,
+		0.0f, 1.0f, 0xFFFFFFFF, 0.0f, 0.0f,
+		1.0f, 1.0f, 0xFFFFFFFF, 0.0f, 0.0f,
 		0.0f, 0.0f, 0xFFFFFFFF, 0.0f, 0.0f);
 	AEGfxTriAdd(
 		0.0f, 0.0f, 0xFFFFFFFF, 0.0f, 0.0f,
-		300.0f, 0.0f, 0xFFFFFFFF, 0.0f, 0.0f,
-		300.0f, 75.0f, 0xFFFFFFFF, 0.0f, 0.0f);
+		1.0f, 0.0f, 0xFFFFFFFF, 0.0f, 0.0f,
+		1.0f, 1.0f, 0xFFFFFFFF, 0.0f, 0.0f);
 	// Saving the mesh (list of triangles) in pMesh
 	rectmesh = AEGfxMeshEnd();
 
@@ -24,28 +24,31 @@ void draw_level_init() {
 
 void draw_level() {
 	AEGfxSetRenderMode(AE_GFX_RM_COLOR);
-	AEGfxSetBlendMode(AE_GFX_BM_BLEND);
-	AEGfxSetTransparency(1.0f);
+	//AEGfxSetBlendMode(AE_GFX_BM_BLEND);
+	//AEGfxSetTransparency(1.0f);
 	AEGfxTextureSet(rect, 0, 0);
 	AEMtx33 scale = { 0 };
-	AEMtx33Scale(&scale, 1, 1);
+	AEMtx33Scale(&scale, 300.0f, 75.0f);
+
 	AEMtx33 rotate = { 0 };
 	AEMtx33Rot(&rotate, 0);
+
 	AEMtx33 translate = { 0 };
-	AEMtx33Trans(&translate, -500.f, -200.f);
+	AEMtx33Trans(&translate, -WINDOWXLENGTH / 2, -WINDOWXLENGTH / 5);
+
 	AEMtx33 transform = { 0 };
 	AEMtx33Concat(&transform, &rotate, &scale);
 	AEMtx33Concat(&transform, &translate, &transform);
 	AEGfxSetTransform(transform.m);
 	AEGfxMeshDraw(rectmesh, AE_GFX_MDM_TRIANGLES);
 
-	AEMtx33Trans(&translate, -150.f, -100.f);
+	AEMtx33Trans(&translate, (- WINDOWXLENGTH / 10) * 1.5, -WINDOWXLENGTH / 10);
 	AEMtx33Concat(&transform, &rotate, &scale);
 	AEMtx33Concat(&transform, &translate, &transform);
 	AEGfxSetTransform(transform.m);
 	AEGfxMeshDraw(rectmesh, AE_GFX_MDM_TRIANGLES);
 
-	AEMtx33Trans(&translate, 175.f, 50.f);
+	AEMtx33Trans(&translate, (WINDOWXLENGTH / 10) * 1.75, WINDOWXLENGTH / 20);
 	AEMtx33Concat(&transform, &rotate, &scale);
 	AEMtx33Concat(&transform, &translate, &transform);
 	AEGfxSetTransform(transform.m);
